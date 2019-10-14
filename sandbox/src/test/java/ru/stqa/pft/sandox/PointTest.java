0358049a9bf4b54c15ef7cf651d2601edc824adb
PKrupitsa <56315287+PKrupitsa@users.noreply.github.com>
2019-10-14 18:42:47 +0300

    Тест для класса Point(исправления)

--- sandbox/src/test/java/ru/stqa/pft/sandox/PointTest.java
+++ sandbox/src/test/java/ru/stqa/pft/sandox/PointTest.java
@@ -9,6 +9,6 @@ public class PointTest {
     public void testDistance(){
         Point p1 = new Point(0, 1);
         Point p2 = new Point(0, 2);
-        Assert.assertEquals(Point.distance(p1, p2), 1.0);
+        Assert.assertEquals(p1.distance(p2), 2.0);
     }
 }
